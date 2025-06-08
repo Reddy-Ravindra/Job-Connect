d4fc2e8b498671a72c3b6173265d031515020b2e
reddy-ravindra <54063485+Reddy-Ravindra@users.noreply.github.com>
2025-06-08 03:20:03 -0400

    Add protected Dashboard page and route guard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./features/auth/Login";
-import Register from "./features/auth/Register";
-import JobList from "./features/jobs/JobList";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -9,9 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<JobList />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route
           path="/dashboard"
           element={
@@ -20,6 +16,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="*" element={<Login />} />
       </Routes>
     </Router>
   );
