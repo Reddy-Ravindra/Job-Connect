552e5591a9b5bc15e5d700a7e84f26ee7c0b28d6
reddy-ravindra <54063485+Reddy-Ravindra@users.noreply.github.com>
2025-06-08 01:28:05 -0400

    Added ApplicationDbContext with EF Core and relationships

---
+++ backend/JobConnect.Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using JobConnect.Core.Entities;
+
+namespace JobConnect.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {}
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Job> Jobs => Set<Job>();
+    public DbSet<Interest> Interests => Set<Interest>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Unique constraint for interest (1 user can’t mark interest twice)
+        modelBuilder.Entity<Interest>()
+            .HasIndex(i => new { i.UserId, i.JobId })
+            .IsUnique();
+
+        // Relationships
+        modelBuilder.Entity<Job>()
+            .HasOne(j => j.Poster)
+            .WithMany(u => u.PostedJobs)
+            .HasForeignKey(j => j.PosterId);
+
+        modelBuilder.Entity<Interest>()
+            .HasOne(i => i.User)
+            .WithMany(u => u.Interests)
+            .HasForeignKey(i => i.UserId);
+
+        modelBuilder.Entity<Interest>()
+            .HasOne(i => i.Job)
+            .WithMany(j => j.InterestedUsers)
+            .HasForeignKey(i => i.JobId);
+    }
+}
